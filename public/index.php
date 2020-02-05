5f318dba5ac901232016e3723ac121e381b241dd
Thiago Peixoto <tfpeixoto@gmail.com>
2020-02-05 07:25:21 -0300

    Inicio do desenvolvimento

--- public/index.php
+++ public/index.php
@@ -1,22 +1,228 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <title>Partner Bank</title>
-  </head>
-  <body>
-    <h1>Partner Bank</h1>
+  <!-- CSS -->
+  <link rel="stylesheet" href="css/style.css">
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-  </body>
+  <title>Partner Bank</title>
+</head>
+
+<body>
+  <header>
+    <div class="container">
+      <div class="flex-row">
+        <nav class="navbar navbar-expand-lg d-flex justify-content-between">
+          <div class="col-4">
+            <a class="navbar-brand" href="#"><img src="images/marca-partner-bank.png" alt="Partner Bank" /></a>
+          </div>
+
+          <div class="col-8">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navMobile" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Alterna navegação">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse text-right" id="navMobile">
+              <div class="navbar-nav">
+                <a class="nav-item nav-link" href="#">Receber por boleto</a>
+                <a class="nav-item nav-link" href="#">Receber por cartão</a>
+                <a class="nav-item nav-link" href="#">Quero conhecer</a>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </div>
+  </header>
+
+  <section id="banner">
+    <div class="container">
+      <div class="row">
+        <div class="col-4">
+          <h1 class="text-amarelo">O jeito simples e seguro de cuidar dos seus serviços bancários</h1>
+          <p class="lead text-roxo">Desburocratize de vez a gestão financeira da sua empresa com o Partner Bank, única solução com pagamento em D+1</p>
+        </div>
+
+        <div class="col-6">
+          <img src="images/executivo-cliente-partner-bank.png" alt="Executivo cliente da Partner Bank" />
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="frase">
+    <div class="container">
+      <div class="row">
+        <div class="col-10">
+          <p class="lead">Com o Partner Bank é possível automatizar processos, facilitar o acesso aos meios de pagamentos e gerir as cobranças de todos os seus clientes e condôminos.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="institucional">
+    <div class="container">
+      <div class="row">
+        <div class="col-5">
+          <img src="#" alt="#" />
+        </div>
+
+        <div class="col-7">
+          <p>Automatizamos processos financeiros para empresas. Nossa solução atende aos mais variados segmentos e portes de negócio e é ideal para modelos de cobrança recorrente, por meio de boleto bancário.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="beneficios">
+    <div class="container">
+      <div class="row">
+        <div class="col-10">
+          <h2>Tenha o controle em suas mãos</h2>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-3">
+          <img src="#" alt="#" />
+          <p>Abra <strong>convênios de forma totalmente automática</strong> sem a burocracia dos bancos</p>
+        </div>
+        <div class="col-3">
+          <img src="#" alt="#" />
+          <p><strong>Automatize</strong> registros e baixas de boletos</p>
+        </div>
+        <div class="col-3">
+          <img src="#" alt="#" />
+          <p>Realize o pagamento de uma <strong>única tarifa</strong>. Apenas na liquidação</p>
+        </div>
+        <div class="col-3">
+          <img src="#" alt="#" />
+          <p>Receba os créditos dos boletos em sua <strong>conta em D+1</strong></p>
+        </div>
+        <div class="col-3">
+          <img src="#" alt="#" />
+          <p><strong>Taxa 0%</strong> nas baixas e alterações nos boletos</p>
+        </div>
+        <div class="col-3">
+          <img src="#" alt="#" />
+          <p>100% integrado ao sistema <strong>Condomínio21</strong></p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="diferenciais">
+    <div class="container">
+      <div class="row">
+        <div class="col-6">
+          <h2>Um produto novo com DNA líder de mercado</h2>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-3">
+          <img src="#" alt="#">
+          <p>Em conformidade com as leis e regulamentações bancárias. Confiança para fazer mais.</p>
+        </div>
+        <div class="col-3">
+          <img src="#" alt="#">
+          <p>Tecnologia segura e auditada para entregar resultados reais.</p>
+        </div>
+        <div class="col-3">
+          <img src="#" alt="#">
+          <p>Somos uma instituição de pagamento cadastrada no Banco Central.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="equipe">
+    <div class="container">
+      <div class="row">
+        <div class="col-6">
+          <h2>Conheça nossa equipe</h2>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-3">
+          <img src="#" alt="#">
+          <h3>Valter Gomes</h3>
+          <small>CEO</small>
+        </div>
+        <div class="col-3">
+          <img src="#" alt="#">
+          <h3>Rodrigo Monteiro</h3>
+          <small>Presidente do Conselho</small>
+        </div>
+        <div class="col-3">
+          <img src="#" alt="#">
+          <h3>Danilo Frota</h3>
+          <small>CTO</small>
+        </div>
+        <div class="col-3">
+          <img src="#" alt="#">
+          <h3>Flávio Ribeiro</h3>
+          <small>CIO</small>
+        </div>
+        <div class="col-3">
+          <img src="#" alt="#">
+          <h3>Roberson Barros</h3>
+          <small>Conselheiro</small>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section id="cta">
+    <div class="container">
+      <div class="row">
+        <div class="col-10">
+          <h2>Comece a fazer diferente agora</h2>
+          <p class="lead">Para nós, tecnologia não é apenas uma forma de mudar processos. É uma forma de alavancar a transformação que o seu negócio precisa. Fale com nossos especialistas e vamos trabalhar juntos.</p>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-6">
+          <a type="button" class="btn btn-lg" href="#">Quero conhecer</a>
+          <a type="button" class="btn btn-lg" href="#">Quero integrar meu ERP</a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-6">
+          <img src="#" alt="#">
+          <p><strong>São Paulo</strong>
+            Rua Álvaro Anes, 56, 8º andar
+            Pinheiros . São Paulo . CEP: 05421-010</p>
+        </div>
+
+        <div class="col-6">
+          <img src="#" alt="#">
+          <p><strong>Belo Horizonte</strong>
+            Rua Santa Catarina, 1631, 8ºAndar
+            Lourdes . Belo Horizonte . CEP: 30170-081</p>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-12">
+          <hr />
+          <p>© Copyright 2020 Partner Bank . Todos os direitos reservados</p>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+  <script src="js/scripts.min.js" crossorigin="anonymous"></script>
+</body>
+
 </html>
