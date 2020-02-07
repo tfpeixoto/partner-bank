dc9905a5c98a251efa9a2a7b99cb5070aa1852c4
Thiago Peixoto <tfpeixoto@gmail.com>
2020-02-07 01:50:02 -0300

    Estilização da LP principal (wip)

--- public/index.php
+++ public/index.php
@@ -17,20 +17,22 @@
     <div class="container">
       <div class="flex-row">
         <nav class="navbar navbar-expand-lg d-flex justify-content-between">
-          <div class="col-4">
+          <div class="col">
             <a class="navbar-brand" href="#"><img src="images/marca-partner-bank.png" alt="Partner Bank" /></a>
           </div>
 
-          <div class="col-8">
+          <div class="col">
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navMobile" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Alterna navegação">
-              <span class="navbar-toggler-icon"></span>
+              <span class="navbar-toggler-icon">
+                <img src="images/icone-menu.svg" alt="Menu mobile" width="32px" />
+              </span>
             </button>
 
             <div class="collapse navbar-collapse text-right" id="navMobile">
               <div class="navbar-nav">
                 <a class="nav-item nav-link" href="#">Receber por boleto</a>
                 <a class="nav-item nav-link" href="#">Receber por cartão</a>
-                <a class="nav-item nav-link" href="#">Quero conhecer</a>
+                <a class="nav-item btn btn-outline-roxo" href="#" data-toggle="modal" data-target="#ModalContato">Quero conhecer</a>
               </div>
             </div>
           </div>
@@ -41,14 +43,14 @@
 
   <section id="banner">
     <div class="container">
-      <div class="row">
-        <div class="col-4">
-          <h1 class="text-amarelo">O jeito simples e seguro de cuidar dos seus serviços bancários</h1>
-          <p class="lead text-roxo">Desburocratize de vez a gestão financeira da sua empresa com o Partner Bank, única solução com pagamento em D+1</p>
+      <div class="row d-flex justify-content-between align-items-center">
+        <div class="col-12 col-md-5">
+          <h1 class="text-amarelo text-center text-md-left">O jeito simples e seguro de cuidar dos seus serviços bancários</h1>
+          <p class="lead text-italic text-roxo text-center text-md-left">Desburocratize de vez a gestão financeira da sua empresa com o Partner Bank, <span class="font-weight-bold">única solução com pagamento em D+1</span></p>
         </div>
 
-        <div class="col-6">
-          <img src="images/executivo-cliente-partner-bank.png" alt="Executivo cliente da Partner Bank" />
+        <div class="col-12 col-md-7">
+          <img src="images/executivo-cliente-partner-bank.png" alt="Executivo cliente da Partner Bank" class="img-fluid" />
         </div>
       </div>
     </div>
@@ -56,9 +58,9 @@
 
   <section id="frase">
     <div class="container">
-      <div class="row">
-        <div class="col-10">
-          <p class="lead">Com o Partner Bank é possível automatizar processos, facilitar o acesso aos meios de pagamentos e gerir as cobranças de todos os seus clientes e condôminos.</p>
+      <div class="row d-flex justify-content-center">
+        <div class="col-10 col-md-8 text-center pt-4">
+          <p class="lead">Com o <span class="font-weight-bold">Partner Bank</span> é possível automatizar processos, facilitar o acesso aos meios de pagamentos e gerir as cobranças de todos os seus clientes e condôminos.</p>
         </div>
       </div>
     </div>
@@ -66,12 +68,14 @@
 
   <section id="institucional">
     <div class="container">
-      <div class="row">
-        <div class="col-5">
-          <img src="#" alt="#" />
+      <div class="row d-flex justify-content-center justify-content-md-between">
+        <div class="col-11 col-md-4">
+          <div class="instituiconal-imagem">
+            <img src="images/marca-partner-bank-institucional.png" alt="Partner Bank" class="img-fluid mb-3 mb-md-0" />
+          </div>
         </div>
 
-        <div class="col-7">
+        <div class="col-11 col-md-7 d-flex align-items-center text-center text-md-left">
           <p>Automatizamos processos financeiros para empresas. Nossa solução atende aos mais variados segmentos e portes de negócio e é ideal para modelos de cobrança recorrente, por meio de boleto bancário.</p>
         </div>
       </div>
@@ -80,36 +84,53 @@
 
   <section id="beneficios">
     <div class="container">
-      <div class="row">
-        <div class="col-10">
-          <h2>Tenha o controle em suas mãos</h2>
+      <div class="row d-flex justify-content-center">
+        <div class="col col-md-8 pb-5 text-center">
+          <h2 class="text-amarelo">Tenha o controle <span class="text-roxo">em suas mãos</span></h2>
         </div>
       </div>
 
-      <div class="row">
-        <div class="col-3">
-          <img src="#" alt="#" />
-          <p>Abra <strong>convênios de forma totalmente automática</strong> sem a burocracia dos bancos</p>
+      <div class="row d-flex justify-content-center">
+        <div class="col-12 col-md-4">
+          <div class="box-beneficio d-flex justify-content-center align-items-center">
+            <img src="images/icone-convenio.svg" alt="#" />
+            <p>Abra <strong>convênios de forma totalmente automática</strong> sem a burocracia dos bancos</p>
+          </div>
         </div>
-        <div class="col-3">
-          <img src="#" alt="#" />
-          <p><strong>Automatize</strong> registros e baixas de boletos</p>
+
+        <div class="col-12 col-md-4">
+          <div class="box-beneficio d-flex justify-content-center align-items-center">
+            <img src="images/icone-automatiza.svg" alt="#" />
+            <p><strong>Automatize</strong> registros e baixas de boletos</p>
+          </div>
         </div>
-        <div class="col-3">
-          <img src="#" alt="#" />
-          <p>Realize o pagamento de uma <strong>única tarifa</strong>. Apenas na liquidação</p>
+
+        <div class="col-12 col-md-4">
+          <div class="box-beneficio d-flex justify-content-center align-items-center">
+            <img src="images/icone-tarifa.svg" alt="#" />
+            <p>Realize o pagamento de uma <strong>única tarifa</strong>. Apenas na liquidação</p>
+          </div>
         </div>
-        <div class="col-3">
-          <img src="#" alt="#" />
-          <p>Receba os créditos dos boletos em sua <strong>conta em D+1</strong></p>
+
+        <div class="col-12 col-md-4">
+          <div class="box-beneficio d-flex justify-content-center align-items-center">
+            <img src="images/icone-d1.svg" alt="#" />
+            <p>Receba os créditos dos boletos em sua <strong>conta em D+1</strong></p>
+          </div>
         </div>
-        <div class="col-3">
-          <img src="#" alt="#" />
-          <p><strong>Taxa 0%</strong> nas baixas e alterações nos boletos</p>
+
+        <div class="col-12 col-md-4">
+          <div class="box-beneficio d-flex justify-content-center align-items-center">
+            <img src="images/icone-taxa0.svg" alt="#" />
+            <p><strong>Taxa 0%</strong> nas baixas e alterações nos boletos</p>
+          </div>
         </div>
-        <div class="col-3">
-          <img src="#" alt="#" />
-          <p>100% integrado ao sistema <strong>Condomínio21</strong></p>
+
+        <div class="col-12 col-md-4">
+          <div class="box-beneficio d-flex justify-content-center align-items-center">
+            <img src="images/icone-integracao.svg" alt="#" />
+            <p>100% integrado ao sistema <strong>Condomínio21</strong></p>
+          </div>
         </div>
       </div>
     </div>
@@ -117,22 +138,31 @@
 
   <section id="diferenciais">
     <div class="container">
-      <div class="row">
-        <div class="col-6">
-          <h2>Um produto novo com DNA líder de mercado</h2>
+      <div class="row d-flex justify-content-center">
+        <div class="col-10 col-md-6 text-center mb-5">
+          <h2 class="text-roxo">Um produto novo com<br /> <span class="text-amarelo">DNA líder de mercado</span></h2>
         </div>
       </div>
-      <div class="row">
-        <div class="col-3">
-          <img src="#" alt="#">
+
+      <div class="row d-flex justify-content-around">
+        <div class="col-10 col-md-3 text-center box-diferenciais">
+          <div class="d-flex justify-content-center align-items-center mx-auto box-diferenciais-img">
+            <img src="images/icone-leis.svg" alt="#">
+          </div>
           <p>Em conformidade com as leis e regulamentações bancárias. Confiança para fazer mais.</p>
         </div>
-        <div class="col-3">
-          <img src="#" alt="#">
+
+        <div class="col-10 col-md-3 text-center box-diferenciais">
+          <div class="d-flex justify-content-center align-items-center mx-auto box-diferenciais-img">
+            <img src="images/icone-seguranca.svg" alt="#">
+          </div>
           <p>Tecnologia segura e auditada para entregar resultados reais.</p>
         </div>
-        <div class="col-3">
-          <img src="#" alt="#">
+
+        <div class="col-10 col-md-3 text-center box-diferenciais">
+          <div class="d-flex justify-content-center align-items-center mx-auto box-diferenciais-img">
+            <img src="images/icone-bc.svg" alt="#">
+          </div>
           <p>Somos uma instituição de pagamento cadastrada no Banco Central.</p>
         </div>
       </div>
@@ -141,37 +171,48 @@
 
   <section id="equipe">
     <div class="container">
-      <div class="row">
-        <div class="col-6">
-          <h2>Conheça nossa equipe</h2>
+      <div class="row d-flex justify-content-center">
+        <div class="col-12 col-md-6 text-center mb-5">
+          <h2 class="text-amarelo">Conheça nossa equipe</h2>
         </div>
       </div>
 
-      <div class="row">
-        <div class="col-3">
-          <img src="#" alt="#">
+      <div class="row d-flex justify-content-center">
+        <div class="col-12 col-md-3 text-center box-equipe">
+          <img src="images/valter-gomes.png" alt="#" class="mb-0 mb-md-4">
           <h3>Valter Gomes</h3>
-          <small>CEO</small>
+          <p>CEO</p>
+          <a href="#" title="Linkedin Valter Gomes"><img src="images/linkedin-logo.svg" alt="Linkedin" /></a>
         </div>
-        <div class="col-3">
-          <img src="#" alt="#">
+
+        <div class="col-12 col-md-3 text-center box-equipe">
+          <img src="images/rodrigo-monteiro.png" alt="#" class="mb-0 mb-md-4">
           <h3>Rodrigo Monteiro</h3>
-          <small>Presidente do Conselho</small>
+          <p>Presidente do Conselho</p>
+          <a href="#" title="Linkedin Rodrigo Monteiro"><img src="images/linkedin-logo.svg" alt="Linkedin" /></a>
         </div>
-        <div class="col-3">
-          <img src="#" alt="#">
+
+        <div class="col-12 col-md-3 text-center box-equipe">
+          <img src="images/danilo-frota.png" alt="#" class="mb-0 mb-md-4">
           <h3>Danilo Frota</h3>
-          <small>CTO</small>
+          <p>CTO</p>
+          <a href="#" title="Linkedin Danilo Frota"><img src="images/linkedin-logo.svg" alt="Linkedin" /></a>
         </div>
-        <div class="col-3">
-          <img src="#" alt="#">
+      </div>
+
+      <div class="row d-flex justify-content-center">
+        <div class="col-12 col-md-3 text-center box-equipe">
+          <img src="images/flavio-ribeiro.png" alt="#" class="mb-0 mb-md-4">
           <h3>Flávio Ribeiro</h3>
-          <small>CIO</small>
+          <p>CIO</p>
+          <a href="#" title="Linkedin Flávio Ribeiro"><img src="images/linkedin-logo.svg" alt="Linkedin" /></a>
         </div>
-        <div class="col-3">
-          <img src="#" alt="#">
+
+        <div class="col-12 col-md-3 text-center box-equipe">
+          <img src="images/roberson-barros.png" alt="#" class="mb-0 mb-md-4">
           <h3>Roberson Barros</h3>
-          <small>Conselheiro</small>
+          <p>Conselheiro</p>
+          <a href="#" title="Linkedin Roberson Barros"><img src="images/linkedin-logo.svg" alt="Linkedin" /></a>
         </div>
       </div>
     </div>
@@ -179,17 +220,17 @@
 
   <section id="cta">
     <div class="container">
-      <div class="row">
-        <div class="col-10">
-          <h2>Comece a fazer diferente agora</h2>
-          <p class="lead">Para nós, tecnologia não é apenas uma forma de mudar processos. É uma forma de alavancar a transformação que o seu negócio precisa. Fale com nossos especialistas e vamos trabalhar juntos.</p>
+      <div class="row d-flex justify-content-center">
+        <div class="col-12 col-md-8 text-center">
+          <h2 class="text-roxo">Comece a fazer diferente agora</h2>
+          <p class="lead text-roxo">Para nós, tecnologia não é apenas uma forma de mudar processos. É uma forma de alavancar a transformação que o seu negócio precisa. Fale com nossos especialistas e vamos trabalhar juntos.</p>
         </div>
       </div>
 
-      <div class="row">
-        <div class="col-6">
-          <a type="button" class="btn btn-lg" href="#">Quero conhecer</a>
-          <a type="button" class="btn btn-lg" href="#">Quero integrar meu ERP</a>
+      <div class="row d-flex justify-content-center">
+        <div class="col-12 col-md-6 text-center">
+          <a type="button" class="btn btn-lg btn-outline-roxo mb-3 mb-md-0" href="#" data-toggle="modal" data-target="#ModalContato">Quero conhecer</a>
+          <a type="button" class="btn btn-lg btn-outline-roxo mb-3 mb-md-0" href="#" data-toggle="modal" data-target="#ModalParceria">Quero integrar meu ERP</a>
         </div>
       </div>
     </div>
@@ -198,17 +239,17 @@
   <footer>
     <div class="container">
       <div class="row">
-        <div class="col-6">
-          <img src="#" alt="#">
-          <p><strong>São Paulo</strong>
-            Rua Álvaro Anes, 56, 8º andar
+        <div class="col-12 col-md-6 d-flex justify-content-center align-items-center my-5">
+          <img src="images/mapa-sao-paulo.svg" alt="Mapa de São Paulo" class="mr-3" />
+          <p class="text-roxo"><strong>São Paulo</strong><br />
+            Rua Álvaro Anes, 56, 8º andar<br />
             Pinheiros . São Paulo . CEP: 05421-010</p>
         </div>
 
-        <div class="col-6">
-          <img src="#" alt="#">
-          <p><strong>Belo Horizonte</strong>
-            Rua Santa Catarina, 1631, 8ºAndar
+        <div class="col-12 col-md-6 d-flex justify-content-center align-items-center">
+          <img src="images/mapa-minas-gerais.svg" alt="Mapa de Minas Gerais" class="mr-3" />
+          <p class="text-roxo"><strong>Belo Horizonte</strong><br />
+            Rua Santa Catarina, 1631, 8ºAndar<br />
             Lourdes . Belo Horizonte . CEP: 30170-081</p>
         </div>
       </div>
@@ -216,11 +257,32 @@
       <div class="row">
         <div class="col-12">
           <hr />
-          <p>© Copyright 2020 Partner Bank . Todos os direitos reservados</p>
+          <p class="text-roxo text-center">© Copyright 2020 Partner Bank . Todos os direitos reservados</p>
         </div>
       </div>
     </div>
   </footer>
+
+  <!-- Modal -->
+  <div class="modal fade" id="ModalContato" tabindex="-1" role="dialog" aria-labelledby="TituloModalCentralizado" aria-hidden="true">
+    <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="TituloModalCentralizado">Entre em contato</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          ...
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+          <button type="button" class="btn btn-primary">Salvar mudanças</button>
+        </div>
+      </div>
+    </div>
+  </div>
 
   <script src="js/scripts.min.js" crossorigin="anonymous"></script>
 </body>
